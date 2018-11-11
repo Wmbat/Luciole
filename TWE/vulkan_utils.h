b21434091482012b893bd302cf693ef3a5088f28
ambre <ambre.spehner@gmail.com>
2018-11-11 13:12:51 -0500

    Made projects into a shared library/DLL

---
+++ TWE/vulkan_utils.h
@@ -0,0 +1,81 @@
+/*!
+ *  Copyright (C) 2018 Wmbat
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  You should have received a copy of the GNU General Public License
+ *  GNU General Public License for more details.
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VULKAN_PROJECT_VULKAN_UTILS_H
+#define VULKAN_PROJECT_VULKAN_UTILS_H
+
+#include <string>
+#include <stdexcept>
+
+#include <vulkan/vulkan.h>
+
+namespace TWE
+{
+#if defined( NDEBUG )
+    static constexpr bool enable_debug_layers = false;
+#else
+    static constexpr bool enable_debug_layers = true;
+#endif
+
+    /**
+     *  @brief Returns a struct holding the desired object and a bool
+     *  to check whether it's creation was successful.
+     *  @tparam T, The desired object type.
+     */
+    template<typename T>
+    struct vk_return_type
+    {
+        VkResult result_;
+        T value_;
+    };
+
+    /**
+     *  @brief Helper function to check if an object was successfully created. Throws
+     *  an exception if it is not, otherwise, returns the desired object.
+     *  @tparam T, The desired object type.
+     *  @param return_obj, Struct containing the desired object and its creation result.
+     *  @param error_msg, Message to display in case of creation failure.
+     *  @return The successfully creation object.
+     */
+    template<typename T>
+    const T check_vk_return_type_result( const vk_return_type<T>& return_obj, const std::string& error_msg )
+    {
+        if( return_obj.result_ != VK_SUCCESS )
+        {
+            throw std::runtime_error{ error_msg };
+        }
+        else
+        {
+            return std::move( return_obj.value_ );
+        }
+    }
+
+    /**
+     *  @brief Helper func to check if an object was successfully created. Throws
+     *  an exception if it is not.
+     *  @param result, The result to check.
+     *  @param error_msg, The message to display in case of error.
+     */
+    inline void check_vk_return_result( const VkResult result, const std::string& error_msg )
+    {
+        if( result != VK_SUCCESS )
+        {
+            throw std::runtime_error{ error_msg };
+        }
+    }
+}
+
+#endif //VULKAN_PROJECT_VULKAN_UTILS_H
---
+++ source/io/keyboard.cpp
@@ -0,0 +1,63 @@
+/*!
+ *  Copyright (C) 2018 Wmbat
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  You should have received a copy of the GNU General Public License
+ *  GNU General Public License for more details.
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <assert.h>
+
+#include "../../TWE/input_devices/keyboard.h"
+
+#include "../../TWE/console.h"
+
+
+namespace TWE
+{
+    bool keyboard::is_key_pressed( key key_code ) const noexcept
+    {
+        return key_states_[static_cast<size_t>( key_code )];
+    }
+
+    keyboard::key_event keyboard::pop_key_event( ) noexcept
+    {
+        auto event_ret = key_buffer_[head_];
+
+        --num_buffer_;
+        key_buffer_[head_] = key_event{ 0, event_type::invalid };
+
+        head_ = ( head_ + 1 ) % MAX_BUFFER_SIZE_;
+
+        return event_ret;
+    }
+
+    void keyboard::emplace_event( const key_event& event ) noexcept
+    {
+        if( num_buffer_ >= MAX_BUFFER_SIZE_ )
+        {
+            pop_key_event();
+        }
+
+        key_buffer_[tail_] = event;
+
+        key_states_[key_buffer_[tail_].id_] = ( key_buffer_[tail_].type_ == event_type::pressed ) ? true : false;
+
+        ++num_buffer_;
+        tail_ = ( tail_ + 1 ) % MAX_BUFFER_SIZE_;
+    }
+
+    bool keyboard::empty( ) const noexcept
+    {
+        return num_buffer_ == 0;
+    }
+}
