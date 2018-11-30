a365c02e39c19fb0362c3de254135eaaa304d9ef
Hugo Spehner <hugo.spehner@gmail.com>
2018-11-30 16:10:34 -0500

    start of windows support

---
+++ release/CMakeFiles/TWE.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/hugos/Documents/programming/cpp/TWE/source/log.cpp" "C:/Users/hugos/Documents/programming/cpp/TWE/release/CMakeFiles/TWE.dir/source/log.cpp.obj"
+  "C:/Users/hugos/Documents/programming/cpp/TWE/source/renderer.cpp" "C:/Users/hugos/Documents/programming/cpp/TWE/release/CMakeFiles/TWE.dir/source/renderer.cpp.obj"
+  "C:/Users/hugos/Documents/programming/cpp/TWE/source/utilities/basic_error.cpp" "C:/Users/hugos/Documents/programming/cpp/TWE/release/CMakeFiles/TWE.dir/source/utilities/basic_error.cpp.obj"
+  "C:/Users/hugos/Documents/programming/cpp/TWE/source/utilities/vk_error.cpp" "C:/Users/hugos/Documents/programming/cpp/TWE/release/CMakeFiles/TWE.dir/source/utilities/vk_error.cpp.obj"
+  "C:/Users/hugos/Documents/programming/cpp/TWE/source/window.cpp" "C:/Users/hugos/Documents/programming/cpp/TWE/release/CMakeFiles/TWE.dir/source/window.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../external/include"
+  "../external/include/glm"
+  "../external/include/spdlog/include"
+  "../TWE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
